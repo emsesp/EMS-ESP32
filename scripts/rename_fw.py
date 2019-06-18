6cfa03af7d5af0e4a43beeb3f2f7b68db61b9580
proddy <paul.derbyshire@philips.com>
2019-06-18 14:12:09 +0200

    added env

--- scripts/rename_fw.py
+++ scripts/rename_fw.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
-from subprocess import call
-import os
 Import("env")
 
 # see http://docs.platformio.org/en/latest/projectconf/advanced_scripting.html#before-pre-and-after-post-actions
 # env.Replace(PROGNAME="firmware_%s" % defines.get("VERSION"))
-env.Replace(PROGNAME="firmware_%s" % env['BOARD'])
+# print env.Dump()
+env.Replace(PROGNAME="firmware_%s" % env['BOARD'] + "_" + env['PIOENV'])
