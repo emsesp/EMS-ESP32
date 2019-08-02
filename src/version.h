672033d7169699a06bd1de0759277b312280781b
Paul <paul@derbyshire.nl>
2019-08-02 16:06:01 +0200

    rename

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 
 #define APP_NAME "EMS-ESP"
-#define APP_VERSION "1.9.0b_web"
+#define APP_VERSION "1.9.0b1_web"
 #define APP_HOSTNAME "ems-esp"
 #define APP_HELPURL "https://github.com/proddy/EMS-ESP/wiki"
 #define APP_UPDATEURL "https://api.github.com/repos/proddy/EMS-ESP/releases/latest"
