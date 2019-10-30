e5920151b7f94feb582dbffe00685fa905c4516a
proddy <paul@derbyshire.nl>
2019-10-30 22:45:54 +0100

    enable download of beta releases via webui

--- tools/wsemulator/run.sh
+++ tools/wsemulator/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-node $PWD/../webfilesbuilder/node_modules/gulp/bin/gulp.js --silent --cwd $PWD/../webfilesbuilder
+node $PWD/../webfilesbuilder/node_modules/gulp/bin/gulp.js --cwd $PWD/../webfilesbuilder
 
 open -na Google\ Chrome --args --disable-web-security --remote-debugging-port=9222 --user-data-dir="/tmp/chrome_dev" $PWD/../../src/websrc/temp/index.html 
 
