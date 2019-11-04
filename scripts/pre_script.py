6b1e0d496c623af83e7cc65ad00657791acf7ea7
proddy <paul@derbyshire.nl>
2019-11-04 19:16:15 +0100

    build optimizations

--- scripts/pre_script.py
+++ scripts/pre_script.py
@@ -4,10 +4,25 @@ import os
 import re
 Import("env")
 
-def build_web(source, target, env):
-    print("\n** Build web...")
-    call(["gulp", "-f", os.getcwd()+"/tools/webfilesbuilder/gulpfile.js"])
+def build_web():
+    print("** Building web...")
+    env.Execute(
+        "node ./tools/webfilesbuilder/node_modules/gulp/bin/gulp.js --cwd ./tools/webfilesbuilder")
 
+def code_check(source, target, env):
+    print("** Starting cppcheck...")
+    call(["cppcheck", os.getcwd()+"/.", "--force", "--enable=all"])
+    print("\n** Finished cppcheck...\n")
+    print("\n** Starting cpplint...")
+    call(["cpplint", "--extensions=ino,cpp,h", "--filter=-legal/copyright,-build/include,-whitespace",
+          "--linelength=120", "--recursive", "src", "lib/myESP"])
+    print("\n** Finished cpplint...")
+
+
+# build web files
+build_web()
+
+# extract application details
 bag = {}
 exprs = [
     (re.compile(r'^#define APP_VERSION\s+"(\S+)"'), 'app_version'),
@@ -31,16 +46,9 @@ with open('./src/ems-esp.cpp', 'r') as f:
 app_version = bag.get('app_version')
 app_name = bag.get('app_name')
 app_hostname = bag.get('app_hostname')
-
 board = env['BOARD']
 branch = env['PIOENV']
 
-# build the web files
-env.AddPreAction("buildprog", build_web)
-
 # build filename, replacing . with _ for the version
-#env.Replace(PROGNAME="firmware_%s" % branch + "_" + app_version.replace(".", "_"))
-#env.Replace(PROGNAME=app_name + "-" + app_version.replace(".", "_") + "-" + board + "-" + branch)
-env.Replace(PROGNAME=app_name + "-" + app_version.replace(".", "_") + "-" + board)
-
-
+env.Replace(PROGNAME=app_name + "-" +
+            app_version.replace(".", "_") + "-" + board)
