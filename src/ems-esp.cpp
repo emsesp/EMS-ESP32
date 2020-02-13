33a840606d504b9ebb0e514ebaeeba730e7e14b9
Paul <paul@derbyshire.nl>
2020-02-13 11:03:13 +0100

    size of mqtt buffer for dallas sensors fix

--- src/ems-esp.cpp
+++ src/ems-esp.cpp
@@ -556,7 +556,7 @@ void publishSensorValues() {
         return; // no sensors attached
     }
 
-    StaticJsonDocument<600> doc;
+    StaticJsonDocument<400> doc;
     JsonObject              sensors = doc.to<JsonObject>();
 
     bool hasdata     = false;
@@ -574,7 +574,7 @@ void publishSensorValues() {
         return; // nothing to send
     }
 
-    char data[200] = {0};
+    char data[400] = {0};
     serializeJson(doc, data, sizeof(data));
     myDebugLog("Publishing external sensor data via MQTT");
     myESP.mqttPublish(TOPIC_EXTERNAL_SENSORS, data);
