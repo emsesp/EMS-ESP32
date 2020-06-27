b06f3eb41f5b50d17d32e82fa5f33df4fc2dd24c
proddy <paul@derbyshire.nl>
2020-06-27 22:51:19 +0200

    ww comfort setting back to 0, D8, EC - #413

--- src/devices/boiler.cpp
+++ src/devices/boiler.cpp
@@ -692,24 +692,23 @@ void Boiler::set_flow_temp(const uint8_t temperature) {
 }
 
 // 1=hot, 2=eco, 3=intelligent
+// note some boilers do not have this setting, than it's done by thermostat
+// on a RC35 it's by EMSESP::send_write_request(0x37, 0x10, 2, &set, 1, 0); (set is 1,2,3)
 void Boiler::set_warmwater_mode(const uint8_t comfort) {
     uint8_t set;
     if (comfort == 1) {
-        LOG_INFO(F("Setting boiler warm water to hot"));
-        set = 1;
+        LOG_INFO(F("Setting boiler warm water to Hot"));
+        set = 0x00;
     } else if (comfort == 2) {
-        LOG_INFO(F("Setting boiler warm water to eco"));
-        set = 0;
+        LOG_INFO(F("Setting boiler warm water to Eco"));
+        set = 0xD8;
     } else if (comfort == 3) {
-        LOG_INFO(F("Setting boiler warm water to intelligent"));
-        set = 2;
+        LOG_INFO(F("Setting boiler warm water to Intelligent"));
+        set = 0xEC;
     } else {
         return; // do nothing
     }
-    write_command(EMS_TYPE_UBAParameterWW, 9, comfort);
-    // some boilers do not have this setting, than it's done by thermostat
-    // Test for RC35, but not a good way, we are here in boiler context.
-    // EMSESP::send_write_request(0x37, 0x10, 2, &set, 1, 0); // for RC35, maybe work also on RC300
+    write_command(EMS_TYPE_UBAParameterWW, 9, set);
 }
 
 // turn on/off warm water
