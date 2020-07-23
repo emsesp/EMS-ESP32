5cd1c4eb7d2af71fe613bd4f18d356f18c30708c
proddy <paul@derbyshire.nl>
2020-07-23 12:03:25 +0200

    loop() doesn't need to be static anymore

--- src/console.h
+++ src/console.h
@@ -191,8 +191,8 @@ class EMSESPStreamConsole : public uuid::console::StreamConsole, public EMSESPSh
 
 class Console {
   public:
-    static void loop();
-    void        start();
+    void loop();
+    void start();
 
     uuid::log::Level log_level();
 
