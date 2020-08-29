5a87b9a7d8b43b52423071338f4dea54ebbeed86
proddy <paul@derbyshire.nl>
2020-08-29 15:16:36 +0200

    default first web page shows the devices

--- interface/src/project/EMSESP.tsx
+++ interface/src/project/EMSESP.tsx
@@ -21,15 +21,15 @@ class EMSESP extends Component<RouteComponentProps> {
     return (
       <MenuAppBar sectionTitle="Dashboard">
         <Tabs value={this.props.match.url} onChange={this.handleTabChange} variant="fullWidth">
-          <Tab value={`/${PROJECT_PATH}/status`} label="EMS Status" />
           <Tab value={`/${PROJECT_PATH}/devices`} label="Devices & Sensors" />
+          <Tab value={`/${PROJECT_PATH}/status`} label="EMS Status" />
           <Tab value={`/${PROJECT_PATH}/help`} label="EMS-ESP Help" />
         </Tabs>
         <Switch>
-          <AuthenticatedRoute exact path={`/${PROJECT_PATH}/status`} component={EMSESPStatusController} />
           <AuthenticatedRoute exact path={`/${PROJECT_PATH}/devices`} component={EMSESPDevicesController} />
+          <AuthenticatedRoute exact path={`/${PROJECT_PATH}/status`} component={EMSESPStatusController} />
           <AuthenticatedRoute exact path={`/${PROJECT_PATH}/help`} component={EMSESPHelp} />
-          <Redirect to={`/${PROJECT_PATH}/status`} />
+          <Redirect to={`/${PROJECT_PATH}/devices`} />
         </Switch>
       </MenuAppBar>
 
