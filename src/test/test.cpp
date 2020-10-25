9ee95985b71d065179fa86f928fadcd3a3e6c9ba
proddy <paul@derbyshire.nl>
2020-10-25 18:33:54 +0100

    updated tests

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -27,7 +27,7 @@ namespace emsesp {
 // used with the 'test' command, under su/admin
 void Test::run_test(uuid::console::Shell & shell, const std::string & command) {
     if (command == "default") {
-        run_test(shell, "mixing"); // add the default test case here
+        run_test(shell, "general"); // add the default test case here
     }
 
     if (command.empty()) {
@@ -820,8 +820,11 @@ void Test::run_test(uuid::console::Shell & shell, const std::string & command) {
         });
 
         EMSESP::rxservice_.ems_mask(EMSbus::EMS_MASK_BUDERUS);
-
         std::string version("1.2.3");
+
+        // add controller
+        EMSESP::add_device(0x09, 114, version, EMSdevice::Brand::BUDERUS);
+
         EMSESP::add_device(0x28, 160, version, EMSdevice::Brand::BUDERUS); // MM100, WWC
         EMSESP::add_device(0x29, 161, version, EMSdevice::Brand::BUDERUS); // MM200, WWC
         EMSESP::add_device(0x20, 160, version, EMSdevice::Brand::BOSCH);   // MM100
@@ -837,10 +840,10 @@ void Test::run_test(uuid::console::Shell & shell, const std::string & command) {
 
         shell.invoke_command("show");
         shell.invoke_command("call");
-        shell.invoke_command("call mixing info");
+        shell.invoke_command("call mixer info");
         shell.invoke_command("publish");
         shell.invoke_command("show mqtt");
-        shell.invoke_command("call mixing");
+        shell.invoke_command("call mixer");
     }
 
     // finally dump to console
