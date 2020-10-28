c6cadd74f7319450d3197f984018132ab86106fc
MichaelDvP <github@mdvp.de>
2020-10-28 15:53:44 +0100

    fix type names for telegrams xFx

--- src/emsdevice.cpp
+++ src/emsdevice.cpp
@@ -311,7 +311,7 @@ std::string EMSdevice::telegram_type_name(std::shared_ptr<const Telegram> telegr
     }
 
     for (const auto & tf : telegram_functions_) {
-        if ((tf.telegram_type_id_ == telegram->type_id) && ((telegram->type_id & 0xF0) != 0xF0)) {
+        if ((tf.telegram_type_id_ == telegram->type_id) && ((telegram->type_id & 0xFFF0) != 0xF0)) {
             return uuid::read_flash_string(tf.telegram_type_name_);
         }
     }
