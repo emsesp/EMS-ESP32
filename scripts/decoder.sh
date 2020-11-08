9285180f2d13033e45e36e7ce31092666ac8ce29
Paul <paul@derbyshire.nl>
2020-11-08 20:24:18 +0100

    add debug scripts back

---
+++ scripts/decoder.sh
@@ -0,0 +1,4 @@
+# python decoder.py -s -e ../.pio/build/esp8266-local/firmware.elf stackdmp.txt
+
+python decoder.py -p ESP32 -e ../.pio/build/esp32-local/firmware.elf stackdmp.txt -t ~/.platformio/packages/toolchain-xtensa32
+
