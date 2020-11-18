fa55a8458f8f22b521160674a6c3bf298ec16614
proddy <paul@derbyshire.nl>
2020-11-18 11:22:58 +0100

    default web API is off for write commands

--- src/WebSettingsService.h
+++ src/WebSettingsService.h
@@ -36,7 +36,7 @@
 #define EMSESP_DEFAULT_SHOWER_ALERT false
 #define EMSESP_DEFAULT_HIDE_LED false
 #define EMSESP_DEFAULT_DALLAS_PARASITE false
-#define EMSESP_DEFAULT_API_ENABLED true
+#define EMSESP_DEFAULT_API_ENABLED false // turn off, because its insecure
 #define EMSESP_DEFAULT_BOOL_FORMAT 1 // on/off
 #define EMSESP_DEFAULT_ANALOG_ENABLED false
 
