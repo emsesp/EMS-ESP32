3959d01936db6cf9dc3a7b1e6249af4461b3b691
proddy <paul@derbyshire.nl>
2021-01-04 12:43:25 +0100

    max 48 chars

--- interface/src/validators/isHostname.ts
+++ interface/src/validators/isHostname.ts
@@ -1,4 +1,4 @@
-const hostnameLengthRegex = /^.{0,32}$/
+const hostnameLengthRegex = /^.{0,48}$/
 const hostnamePatternRegex = /^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9-]*[A-Za-z0-9])$/
 
 export default function isHostname(hostname: string) {
