856386f45668bfa69b778f7e8d2384c7e6b8132b
MichaelDvP <github@mdvp.de>
2021-03-04 15:37:17 +0100

    WEP2->WPA2

--- interface/src/network/WiFiSecurityModes.ts
+++ interface/src/network/WiFiSecurityModes.ts
@@ -5,14 +5,15 @@ export const isNetworkOpen = ({ encryption_type }: WiFiNetwork) => encryption_ty
 export const networkSecurityMode = ({ encryption_type }: WiFiNetwork) => {
   switch (encryption_type) {
     case WiFiEncryptionType.WIFI_AUTH_WEP:
-    case WiFiEncryptionType.WIFI_AUTH_WEP_PSK:
       return "WEP";
-    case WiFiEncryptionType.WIFI_AUTH_WEP2_PSK:
-      return "WEP2";
+    case WiFiEncryptionType.WIFI_AUTH_WPA_PSK:
+      return "WPA";
+    case WiFiEncryptionType.WIFI_AUTH_WPA2_PSK:
+      return "WPA2";
     case WiFiEncryptionType.WIFI_AUTH_WPA_WPA2_PSK:
-      return "WPA/WEP2";
+      return "WPA/WPA2";
     case WiFiEncryptionType.WIFI_AUTH_WPA2_ENTERPRISE:
-      return "WEP2 Enterprise";
+      return "WPA2 Enterprise";
     case WiFiEncryptionType.WIFI_AUTH_OPEN:
       return "None";
     default:
