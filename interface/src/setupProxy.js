47a401b66e60a4e92a04612156aec617484ac34e
Proddy <paul@derbyshire.nl>
2021-04-10 16:07:10 +0200

    update npm & typescript

--- interface/src/setupProxy.js
+++ interface/src/setupProxy.js
@@ -1,4 +1,4 @@
-const createProxyMiddleware = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
     app.use(
@@ -6,10 +6,7 @@ module.exports = function (app) {
         createProxyMiddleware({
             target: 'http://localhost:3080',
             secure: false,
-            changeOrigin: true,
-            // pathRewrite: {
-            //     '^/api/settings': '/api/app/settings'
-            // },
+            changeOrigin: true
         })
     );
 };
