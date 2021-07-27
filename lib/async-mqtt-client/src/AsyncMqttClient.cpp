c5688ab632948464a1114f79c956c2ab595a58d7
proddy <paul@derbyshire.nl>
2021-07-27 21:43:17 +0200

    manually merge in official 0.9.0 version

--- lib/async-mqtt-client/src/AsyncMqttClient.cpp
+++ lib/async-mqtt-client/src/AsyncMqttClient.cpp
@@ -6,7 +6,7 @@ AsyncMqttClient::AsyncMqttClient()
 , _tail(nullptr)
 , _sent(0)
 , _state(DISCONNECTED)
-, _tlsBadFingerprint(false)
+, _disconnectReason(AsyncMqttClientDisconnectReason::TCP_DISCONNECTED)
 , _lastClientActivity(0)
 , _lastServerActivity(0)
 , _lastPingRequestTime(0)
@@ -194,7 +194,6 @@ void AsyncMqttClient::_freeCurrentParsedPacket() {
 
 void AsyncMqttClient::_clear() {
   _lastPingRequestTime = 0;
-  _tlsBadFingerprint = false;
   _freeCurrentParsedPacket();
   _clearQueue(true);  // keep session data for now
 
@@ -219,7 +218,7 @@ void AsyncMqttClient::_onConnect() {
     }
 
     if (!sslFoundFingerprint) {
-      _tlsBadFingerprint = true;
+      _disconnectReason = AsyncMqttClientDisconnectReason::TLS_BAD_FINGERPRINT;
       _client.close(true);
       return;
     }
@@ -243,17 +242,10 @@ void AsyncMqttClient::_onConnect() {
 void AsyncMqttClient::_onDisconnect() {
   log_i("TCP disconn");
   _state = DISCONNECTED;
-  AsyncMqttClientDisconnectReason reason;
-
-  if (_tlsBadFingerprint) {
-    reason = AsyncMqttClientDisconnectReason::TLS_BAD_FINGERPRINT;
-  } else {
-    reason = AsyncMqttClientDisconnectReason::TCP_DISCONNECTED;
-  }
 
   _clear();
 
-  for (auto callback : _onDisconnectUserCallbacks) callback(reason);
+  for (auto callback : _onDisconnectUserCallbacks) callback(_disconnectReason);
 }
 
 /*
@@ -541,6 +533,8 @@ void AsyncMqttClient::_onConnAck(bool sessionPresent, uint8_t connectReturnCode)
     for (auto callback : _onConnectUserCallbacks) callback(sessionPresent);
   } else {
     // Callbacks are handled by the onDisconnect function which is called from the AsyncTcp lib
+    _disconnectReason = static_cast<AsyncMqttClientDisconnectReason>(connectReturnCode);
+    return;
   }
   _handleQueue();  // send any remaining data from continued session
 }
@@ -710,6 +704,7 @@ void AsyncMqttClient::connect() {
   if (_state != DISCONNECTED) return;
   log_i("CONNECTING");
   _state = CONNECTING;
+  _disconnectReason = AsyncMqttClientDisconnectReason::TCP_DISCONNECTED;  // reset any previous
 
   _client.setRxTimeout(_keepAlive);
 
@@ -768,6 +763,12 @@ uint16_t AsyncMqttClient::publish(const char* topic, uint8_t qos, bool retain, c
   AsyncMqttClientInternals::OutPacket* msg = new AsyncMqttClientInternals::PublishOutPacket(topic, qos, retain, payload, length);
   _addBack(msg);
   return msg->packetId();
+}
+
+bool AsyncMqttClient::clearQueue() {
+  if (_state != DISCONNECTED) return false;
+  _clearQueue(false);
+  return true;
 }
 
 const char* AsyncMqttClient::getClientId() const {
--- lib/async-mqtt-client/src/AsyncMqttClient.hpp
+++ lib/async-mqtt-client/src/AsyncMqttClient.hpp
@@ -82,6 +82,7 @@ class AsyncMqttClient {
   uint16_t subscribe(const char* topic, uint8_t qos);
   uint16_t unsubscribe(const char* topic);
   uint16_t publish(const char* topic, uint8_t qos, bool retain, const char* payload = nullptr, size_t length = 0, bool dup = false, uint16_t message_id = 0);
+  bool clearQueue();  // Not MQTT compliant!
 
   const char* getClientId() const;
 
@@ -96,7 +97,7 @@ class AsyncMqttClient {
     DISCONNECTING,
     DISCONNECTED
   } _state;
-  bool _tlsBadFingerprint;
+  AsyncMqttClientDisconnectReason _disconnectReason;
   uint32_t _lastClientActivity;
   uint32_t _lastServerActivity;
   uint32_t _lastPingRequestTime;
