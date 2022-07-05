eddbacf5a718fece44bba64b389b2b893dca5a3d
proddy <paul@derbyshire.nl>
2022-07-05 13:00:58 +0200

    fix column size for temperature

--- interface/src/project/DashboardData.tsx
+++ interface/src/project/DashboardData.tsx
@@ -162,7 +162,7 @@ const DashboardData: FC = () => {
     common_theme,
     {
       Table: `
-        --data-table-library_grid-template-columns: repeat(1, minmax(0, 1fr)) 100px 40px;
+        --data-table-library_grid-template-columns: repeat(1, minmax(0, 1fr)) 140px 40px;
       `,
       BaseRow: `
         .td {
