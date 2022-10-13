5a0f4c14627062d6eda5138eb6f28cf92f3f2abb
pswid <78219494+pswid@users.noreply.github.com>
2022-10-13 19:43:03 +0200

    The names of the entities have been translated into Polish
    not 100% complete

--- interface/src/project/SettingsApplication.tsx
+++ interface/src/project/SettingsApplication.tsx
@@ -347,9 +347,7 @@ const SettingsApplication: FC = () => {
             <MenuItem value="de">Deutsch (DE)</MenuItem>
             <MenuItem value="nl">Nederlands (NL)</MenuItem>
             <MenuItem value="se">Svenska (SE)</MenuItem>
-            <MenuItem disabled value="pl">
-              Polski (PL)
-            </MenuItem>
+            <MenuItem value="pl">Polski (PL)</MenuItem>
             <MenuItem disabled value="no">
               Norsk (NO)
             </MenuItem>
