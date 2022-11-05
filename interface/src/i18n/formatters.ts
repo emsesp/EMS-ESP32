a2ee2a5e6bc84140ca5a48cb74e8df6e33e4733e
Proddy <paul@derbyshire.nl>
2022-11-05 12:25:14 +0100

    remove custom formatter

--- interface/src/i18n/formatters.ts
+++ interface/src/i18n/formatters.ts
@@ -1,10 +1,9 @@
 import type { FormattersInitializer } from 'typesafe-i18n';
 import type { Locales, Formatters } from './i18n-types';
-import { date } from 'typesafe-i18n/formatters';
 
 export const initFormatters: FormattersInitializer<Locales, Formatters> = (locale: Locales) => {
   const formatters: Formatters = {
-    weekday: date(locale, { weekday: 'long' })
+    // add your formatter functions here
   };
 
   return formatters;
