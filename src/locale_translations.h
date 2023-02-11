ffacefb792aacfc8d3e589cc6ef3701c883b799e
Proddy <paul@derbyshire.nl>
2023-02-11 19:06:02 +0100

    update copyright year

--- src/locale_translations.h
+++ src/locale_translations.h
@@ -1,6 +1,6 @@
 ﻿/*
  * EMS-ESP - https://github.com/emsesp/EMS-ESP
- * Copyright 2020  Paul Derbyshire
+ * Copyright 2020-2023  Paul Derbyshire
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
