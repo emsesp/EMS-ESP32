7eef95f57c45d3469b699eb435d1e35b32731cde
Proddy <paul@derbyshire.nl>
2023-03-14 21:58:54 +0100

    lint warning fix

--- interface/src/framework/security/UserForm.tsx
+++ interface/src/framework/security/UserForm.tsx
@@ -1,4 +1,4 @@
-import { FC, useState, useEffect } from 'react';
+import React, { FC, useState, useEffect } from 'react';
 import Schema, { ValidateFieldsError } from 'async-validator';
 
 import CancelIcon from '@mui/icons-material/Cancel';
