ab4f8d46ada611c3c90da1c86b0d5f4edeb61175
Proddy <paul@derbyshire.nl>
2023-07-03 11:44:23 +0200

    fix standalone build

--- lib_standalone/espMqttClient.h
+++ lib_standalone/espMqttClient.h
@@ -7,51 +7,40 @@
 namespace espMqttClientTypes {
 
 enum class DisconnectReason : uint8_t {
-  USER_OK = 0,
-  MQTT_UNACCEPTABLE_PROTOCOL_VERSION = 1,
-  MQTT_IDENTIFIER_REJECTED = 2,
-  MQTT_SERVER_UNAVAILABLE = 3,
-  MQTT_MALFORMED_CREDENTIALS = 4,
-  MQTT_NOT_AUTHORIZED = 5,
-  TLS_BAD_FINGERPRINT = 6,
-  TCP_DISCONNECTED = 7
+    USER_OK                            = 0,
+    MQTT_UNACCEPTABLE_PROTOCOL_VERSION = 1,
+    MQTT_IDENTIFIER_REJECTED           = 2,
+    MQTT_SERVER_UNAVAILABLE            = 3,
+    MQTT_MALFORMED_CREDENTIALS         = 4,
+    MQTT_NOT_AUTHORIZED                = 5,
+    TLS_BAD_FINGERPRINT                = 6,
+    TCP_DISCONNECTED                   = 7
 };
-const char* disconnectReasonToString(DisconnectReason reason);
+const char * disconnectReasonToString(DisconnectReason reason);
 
-enum class SubscribeReturncode : uint8_t {
-  QOS0 = 0x00,
-  QOS1 = 0x01,
-  QOS2 = 0x02,
-  FAIL = 0X80
-};
-const char* subscribeReturncodeToString(SubscribeReturncode returnCode);
+enum class SubscribeReturncode : uint8_t { QOS0 = 0x00, QOS1 = 0x01, QOS2 = 0x02, FAIL = 0X80 };
+const char * subscribeReturncodeToString(SubscribeReturncode returnCode);
 
-enum class Error : uint8_t {
-  SUCCESS = 0,
-  OUT_OF_MEMORY = 1,
-  MAX_RETRIES = 2,
-  MALFORMED_PARAMETER = 3,
-  MISC_ERROR = 4
-};
-const char* errorToString(Error error);
+enum class Error : uint8_t { SUCCESS = 0, OUT_OF_MEMORY = 1, MAX_RETRIES = 2, MALFORMED_PARAMETER = 3, MISC_ERROR = 4 };
+const char * errorToString(Error error);
 
 struct MessageProperties {
-  uint8_t qos;
-  bool dup;
-  bool retain;
-  uint16_t packetId;
+    uint8_t  qos;
+    bool     dup;
+    bool     retain;
+    uint16_t packetId;
 };
 
-typedef std::function<void(bool sessionPresent)> OnConnectCallback;
-typedef std::function<void(DisconnectReason reason)> OnDisconnectCallback;
-typedef std::function<void(uint16_t packetId, const SubscribeReturncode* returncodes, size_t len)> OnSubscribeCallback;
-typedef std::function<void(uint16_t packetId)> OnUnsubscribeCallback;
-typedef std::function<void(const MessageProperties& properties, const char* topic, const uint8_t* payload, size_t len, size_t index, size_t total)> OnMessageCallback;
-typedef std::function<void(uint16_t packetId)> OnPublishCallback;
-typedef std::function<size_t(uint8_t* data, size_t maxSize, size_t index)> PayloadCallback;
-typedef std::function<void(uint16_t packetId, Error error)> OnErrorCallback;
+typedef std::function<void(bool sessionPresent)>                                                    OnConnectCallback;
+typedef std::function<void(DisconnectReason reason)>                                                OnDisconnectCallback;
+typedef std::function<void(uint16_t packetId, const SubscribeReturncode * returncodes, size_t len)> OnSubscribeCallback;
+typedef std::function<void(uint16_t packetId)>                                                      OnUnsubscribeCallback;
+typedef std::function<void(const MessageProperties & properties, const char * topic, const uint8_t * payload, size_t len, size_t index, size_t total)> OnMessageCallback;
+typedef std::function<void(uint16_t packetId)>                              OnPublishCallback;
+typedef std::function<size_t(uint8_t * data, size_t maxSize, size_t index)> PayloadCallback;
+typedef std::function<void(uint16_t packetId, Error error)>                 OnErrorCallback;
 
-}
+} // namespace espMqttClientTypes
 
 class espMqttClient {
   public:
@@ -107,6 +96,10 @@ class espMqttClient {
 
     const char * getClientId() {
         return "12";
+    }
+
+    uint16_t getQueue() const {
+        return 0;
     }
 
   private:
