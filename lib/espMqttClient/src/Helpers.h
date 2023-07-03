5977b0585d70f049af2f7a45bac6e5dc2f9b9a25
MichaelDvP <github@mdvp.de>
2023-07-03 21:09:56 +0200

    update mqtClient

--- lib/espMqttClient/src/Helpers.h
+++ lib/espMqttClient/src/Helpers.h
@@ -16,7 +16,7 @@ the LICENSE file.
   #define EMC_SEMAPHORE_TAKE() xSemaphoreTake(_xSemaphore, portMAX_DELAY)
   #define EMC_SEMAPHORE_GIVE() xSemaphoreGive(_xSemaphore)
   #define EMC_GET_FREE_MEMORY() std::max(ESP.getMaxAllocHeap(), ESP.getMaxAllocPsram())
-  #define EMC_YIELD() taskYIELD()
+  #define EMC_YIELD() vTaskDelay(1)
   #define EMC_GENERATE_CLIENTID(x) snprintf(x, EMC_CLIENTID_LENGTH, "esp32%06llx", ESP.getEfuseMac());
 #elif defined(ARDUINO_ARCH_ESP8266)
   #include <Arduino.h>  // millis(), ESP.getFreeHeap();
