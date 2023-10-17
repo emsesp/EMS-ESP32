5be1482c20841594d0c3fb4dc4b398e4ad0fc04b
proddy <paul@derbyshire.nl>
2023-10-17 13:35:25 +0200

    tidy up package builds

--- interface/vite.config.ts
+++ interface/vite.config.ts
@@ -1,58 +1,18 @@
-import { defineConfig, type PluginOption } from 'vite';
+import { defineConfig } from 'vite';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
-import svgrPlugin from 'vite-plugin-svgr';
-import { visualizer } from 'rollup-plugin-visualizer';
-import ProgmemGenerator from './progmem-generator';
 import preact from '@preact/preset-vite';
+import viteImagemin from 'vite-plugin-imagemin';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig(({ command, mode }) => {
-  if (mode === 'hosted') {
+  // standalone build for development - runs the server
+  if (command === 'serve') {
+    console.log('Building for standalone');
     return {
-      // hosted, ignore all errors, output to dist
-      plugins: [preact(), viteTsconfigPaths(), svgrPlugin(), visualizer({ gzipSize: true }) as PluginOption]
-    };
-  } else {
-    // normal build
-    return {
-      plugins: [
-        preact(),
-        viteTsconfigPaths(),
-        svgrPlugin(),
-        ProgmemGenerator({ outputPath: '../lib/framework/WWWData.h', bytesPerLine: 20 })
-      ],
-
-      build: {
-        outDir: 'build',
-        chunkSizeWarningLimit: 1024,
-        sourcemap: false,
-        manifest: false,
-        minify: mode === 'development' ? false : 'terser',
-        rollupOptions: {
-          /**
-           * Ignore "use client" waning since we are not using SSR
-           */
-          onwarn(warning, warn) {
-            if (warning.code === 'MODULE_LEVEL_DIRECTIVE' && warning.message.includes(`"use client"`)) {
-              return;
-            }
-            warn(warning);
-          }
-        }
-      },
-
-      onwarn(warning, warn) {
-        if (warning.code === 'MODULE_LEVEL_DIRECTIVE') {
-          return;
-        }
-        warn(warning);
-      },
-
+      plugins: [preact(), viteTsconfigPaths(), visualizer()],
       server: {
         open: true,
-        port: 3000,
-        // watch: {
-        //   usePolling: true
-        // },
+        port: mode === 'production' ? 4173 : 3000,
         proxy: {
           '/rest': 'http://localhost:3080',
           '/api': {
@@ -64,6 +24,100 @@ export default defineConfig(({ command, mode }) => {
             target: 'http://localhost:3080',
             changeOrigin: true,
             secure: false
+          }
+        }
+      }
+    };
+  }
+
+  // production build, both for hosted and building the firmware
+  if (command === 'build') {
+    console.log('Building for production, mode ' + mode);
+    return {
+      plugins: [
+        preact(),
+        viteTsconfigPaths(),
+        {
+          ...viteImagemin({
+            verbose: false,
+            gifsicle: {
+              optimizationLevel: 7,
+              interlaced: false
+            },
+            optipng: {
+              optimizationLevel: 7
+            },
+            mozjpeg: {
+              quality: 20
+            },
+            pngquant: {
+              quality: [0.8, 0.9],
+              speed: 4
+            },
+            svgo: {
+              plugins: [
+                {
+                  name: 'removeViewBox'
+                },
+                {
+                  name: 'removeEmptyAttrs',
+                  active: false
+                }
+              ]
+            }
+          }),
+          enforce: 'pre'
+        }
+      ],
+
+      build: {
+        outDir: mode === 'hosted' ? 'dist' : 'build',
+        reportCompressedSize: false,
+        chunkSizeWarningLimit: 1024,
+        sourcemap: false,
+        manifest: false,
+        minify: 'terser',
+        terserOptions: {
+          parse: {
+            // parse options
+          },
+          compress: {
+            // compress options
+            passes: 4,
+            arrows: true,
+            drop_console: true,
+            sequences: true
+          },
+          mangle: {
+            // mangle options
+            eval: true,
+            properties: {
+              // mangle property options
+            }
+          },
+          format: {
+            // format options (can also use `output` for backwards compatibility)
+          },
+          sourceMap: {
+            // source map options
+          },
+          ecma: 5,
+          enclose: false,
+          keep_classnames: false,
+          keep_fnames: false,
+          ie8: false,
+          module: false,
+          nameCache: null,
+          safari10: false,
+          toplevel: false
+        },
+        rollupOptions: {
+          // Ignore "use client" waning since we are not using SSR
+          onwarn(warning, warn) {
+            if (warning.code === 'MODULE_LEVEL_DIRECTIVE' && warning.message.includes(`"use client"`)) {
+              return;
+            }
+            warn(warning);
           }
         }
       }
--- scripts/build_interface.py
+++ scripts/build_interface.py
@@ -1,32 +1,7 @@
 from pathlib import Path
-from shutil import copytree, rmtree, copyfileobj
 import os
-import gzip
-
-# brotli has better compression than gzip but requires https so leaving here for future
-# import brotli
 
 Import("env")
-
-
-def gzipFile(file):
-    with open(file, 'rb') as f_in:
-        with gzip.open(file + '.gz', 'wb') as f_out:
-            copyfileobj(f_in, f_out)
-    os.remove(file)
-
-    # brotli version:
-    # with open(file + '.br', 'wb') as f_out:
-    #     with open(file, 'rb') as f_in:
-    #         f_out.write(brotli.compress(f_in.read(), quality=11))
-    #     os.remove(file)
-
-
-def flagExists(flag):
-    buildFlags = env.ParseFlags(env["BUILD_FLAGS"])
-    for define in buildFlags.get("CPPDEFINES"):
-        if (define == flag or (isinstance(define, list) and define[0] == flag)):
-            return True
 
 
 def buildWeb():
@@ -39,23 +14,12 @@ def buildWeb():
             text = r.read().replace("Locales = 'pl'", "Locales = 'en'")
         with open("./src/i18n/i18n-util.ts", "w") as w:
             w.write(text)
-        print("Setting locale to 'en'")
+        print("Setting WebUI locale to 'en'")
         env.Execute("yarn run build")
-
-        buildPath = Path("build")
-        wwwPath = Path("../data/www")
-
-        if wwwPath.exists() and wwwPath.is_dir():
-            rmtree(wwwPath)
-
-        print("Copying web files from build to data/www...")
-        copytree(buildPath, wwwPath)
-        for currentpath, folders, files in os.walk(wwwPath):
-            for file in files:
-                gzipFile(os.path.join(currentpath, file))
-
+        env.Execute("yarn run webUI")
     finally:
         os.chdir("..")
+
 
 if not (env.IsCleanTarget()):
     buildWeb()
