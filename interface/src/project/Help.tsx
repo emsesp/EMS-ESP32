1cbfc91912a760fa473b63ffb4069983df27b1d8
Proddy <paul@derbyshire.nl>
2023-11-26 15:56:30 +0100

    remove tab from help page

--- interface/src/project/Help.tsx
+++ interface/src/project/Help.tsx
@@ -1,28 +1,83 @@
-import { Tab } from '@mui/material';
-import { Navigate, Route, Routes } from 'react-router-dom';
-import HelpInformation from './HelpInformation';
+import CommentIcon from '@mui/icons-material/CommentTwoTone';
+import EastIcon from '@mui/icons-material/East';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import MenuBookIcon from '@mui/icons-material/MenuBookTwoTone';
+import { Box, List, ListItem, ListItemAvatar, ListItemText, Link, Typography } from '@mui/material';
 import type { FC } from 'react';
-
-import { RouterTabs, useRouterTab, useLayoutTitle } from 'components';
-
+import { SectionContent, useLayoutTitle } from 'components';
 import { useI18nContext } from 'i18n/i18n-react';
 
 const Help: FC = () => {
   const { LL } = useI18nContext();
-  const { routerTab } = useRouterTab();
-
   useLayoutTitle(LL.HELP_OF(''));
 
+  const uploadURL = window.location.origin + '/system/upload';
+
   return (
-    <>
-      <RouterTabs value={routerTab}>
-        <Tab value="information" label={LL.HELP_OF('EMS-ESP')} />
-      </RouterTabs>
-      <Routes>
-        <Route path="information" element={<HelpInformation />} />
-        <Route path="/*" element={<Navigate replace to="information" />} />
-      </Routes>
-    </>
+    <SectionContent title={LL.SUPPORT_INFORMATION()} titleGutter>
+      <List>
+        <ListItem>
+          <ListItemAvatar>
+            <MenuBookIcon style={{ fontSize: 24, color: 'lightblue', verticalAlign: 'middle' }} />
+          </ListItemAvatar>
+          <ListItemText>
+            {LL.HELP_INFORMATION_1()}&nbsp;
+            <EastIcon style={{ fontSize: 24, color: 'lightblue', verticalAlign: 'middle' }} />
+            &nbsp;
+            <Link target="_blank" href="https://emsesp.github.io/docs" color="primary">
+              {LL.CLICK_HERE()}
+            </Link>
+          </ListItemText>
+        </ListItem>
+
+        <ListItem>
+          <ListItemAvatar>
+            <CommentIcon style={{ fontSize: 24, color: 'lightblue', verticalAlign: 'middle' }} />
+          </ListItemAvatar>
+          <ListItemText>
+            {LL.HELP_INFORMATION_2()}&nbsp;
+            <EastIcon style={{ fontSize: 24, color: 'lightblue', verticalAlign: 'middle' }} />
+            &nbsp;
+            <Link target="_blank" href="https://discord.gg/3J3GgnzpyT" color="primary">
+              {LL.CLICK_HERE()}
+            </Link>
+          </ListItemText>
+        </ListItem>
+
+        <ListItem>
+          <ListItemAvatar>
+            <GitHubIcon style={{ fontSize: 24, color: 'lightblue', verticalAlign: 'middle' }} />
+          </ListItemAvatar>
+          <ListItemText>
+            {LL.HELP_INFORMATION_3()}&nbsp;
+            <EastIcon style={{ fontSize: 24, color: 'lightblue', verticalAlign: 'middle' }} />
+            <Link target="_blank" href="https://github.com/emsesp/EMS-ESP32/issues/new/choose" color="primary">
+              {LL.CLICK_HERE()}
+            </Link>
+            <br />
+            <i>({LL.HELP_INFORMATION_4()}</i>&nbsp;
+            <Link href={uploadURL} color="primary">
+              {LL.UPLOAD()}
+            </Link>
+            )
+          </ListItemText>
+        </ListItem>
+      </List>
+
+      <Box border={1} p={1} mt={4} color="orange">
+        <Typography align="center" variant="subtitle1" color="orange">
+          <b>{LL.HELP_INFORMATION_5()}</b>
+        </Typography>
+        <Typography align="center">
+          <Link target="_blank" href="https://github.com/emsesp/EMS-ESP32" color="primary">
+            {'github.com/emsesp/EMS-ESP32'}
+          </Link>
+        </Typography>
+        <Typography color="white" align="center">
+          @proddy @MichaelDvP
+        </Typography>
+      </Box>
+    </SectionContent>
   );
 };
 
