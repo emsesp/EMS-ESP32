faa21abe54bfea104116189756d56aeb1ab11c8a
Proddy <paul@derbyshire.nl>
2023-12-10 14:50:34 +0100

    revert back to absolute routing

--- interface/src/components/routing/useRouterTab.ts
+++ interface/src/components/routing/useRouterTab.ts
@@ -1,12 +1,12 @@
-import { useMatch, useResolvedPath } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 export const useRouterTab = () => {
-  // const loc = useLocation().pathname;
-  // const routerTab = loc.substring(0, loc.lastIndexOf('/')) ? loc : false;
+  const loc = useLocation().pathname;
+  const routerTab = loc.substring(0, loc.lastIndexOf('/')) ? loc : false;
 
-  const routerTabPath = useResolvedPath(':tab');
-  const routerTabPathMatch = useMatch(routerTabPath.pathname);
-  const routerTab = routerTabPathMatch?.params?.tab || false;
+  // const routerTabPath = useResolvedPath(':tab');
+  // const routerTabPathMatch = useMatch(routerTabPath.pathname);
+  // const routerTab = routerTabPathMatch?.params?.tab || false;
 
   return { routerTab } as const;
 };
