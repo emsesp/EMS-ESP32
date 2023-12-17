854a39fe6bd69e16b15914e819729097002e5ed8
MichaelDvP <github@mdvp.de>
2023-12-17 12:35:07 +0100

    ethernet working with arduino_3.0

--- src/system.cpp
+++ src/system.cpp
@@ -732,7 +732,11 @@ void System::network_init(bool refresh) {
     //  ETH_CLOCK_GPIO17_OUT = 3  RMII clock output from GPIO17, for 50hz inverted clock
     auto clock_mode = (eth_clock_mode_t)eth_clock_mode_;
 
-    eth_present_ = ETH.begin((eth_phy_type_t)phy_addr, power, mdc, mdio, type, clock_mode);
+#if ESP_ARDUINO_VERSION_MAJOR < 3
+    eth_present_ = ETH.begin(phy_addr, power, mdc, mdio, type, clock_mode);
+#else
+    eth_present_ = ETH.begin(type, phy_addr, mdc, mdio, power, clock_mode);
+#endif
 #endif
 }
 
