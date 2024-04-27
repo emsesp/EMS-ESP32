c07dc899de3f7e597d0b1144a5a286f3f3fe6f50
proddy <paul@derbyshire.nl>
2024-04-27 13:00:32 +0200

    added LanguageSelector

--- interface/src/components/inputs/index.ts
+++ interface/src/components/inputs/index.ts
@@ -1,3 +1,4 @@
 export { default as BlockFormControlLabel } from './BlockFormControlLabel';
 export { default as ValidatedPasswordField } from './ValidatedPasswordField';
 export { default as ValidatedTextField } from './ValidatedTextField';
+export { default as LanguageSelector } from './LanguageSelector';
