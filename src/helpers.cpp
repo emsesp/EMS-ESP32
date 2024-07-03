12a49427143e901c62c7a6e6873086392b8d32b6
proddy <paul@derbyshire.nl>
2024-07-03 22:39:41 +0200

    minor updates for modbus

--- src/helpers.cpp
+++ src/helpers.cpp
@@ -837,7 +837,7 @@ uint16_t Helpers::string2minutes(const std::string & str) {
     }
 }
 
-float Helpers::numericoperator2scalefactor(uint8_t numeric_operator) {
+float Helpers::numericoperator2scalefactor(int8_t numeric_operator) {
     if (numeric_operator == 0)
         return 1.0f;
     else if (numeric_operator > 0)
