0edb5c0fd9190361fe66447a5a4aac1fdd8736d7
proddy <paul@derbyshire.nl>
2024-07-27 15:04:22 +0200

    updates to scheduler for immediate commands #1893

--- src/helpers.cpp
+++ src/helpers.cpp
@@ -411,7 +411,8 @@ uint32_t Helpers::hextoint(const char * hex) {
         // get current character then increment
         char byte = *hex++;
         // transform hex character to the 4bit equivalent number, using the ascii table indexes
-        if (byte == ' ') byte = *hex++; // skip spaces
+        if (byte == ' ')
+            byte = *hex++; // skip spaces
         if (byte >= '0' && byte <= '9')
             byte = byte - '0';
         else if (byte >= 'a' && byte <= 'f')
