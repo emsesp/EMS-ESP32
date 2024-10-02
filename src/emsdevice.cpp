f9f87ddc0e0e35283253809deb41d51b81e7e9ea
proddy <paul@derbyshire.nl>
2024-10-02 18:53:36 +0200

    add comment

--- src/emsdevice.cpp
+++ src/emsdevice.cpp
@@ -1022,7 +1022,7 @@ void EMSdevice::generate_values_web(JsonObject output) {
 }
 
 // as generate_values_web() but stripped down to only show all entities and their state
-// this is used only for WebCustomizationService::device_entities()
+// this is used only for WebCustomizationService::device_entities() (rest/deviceEntities?id=n)
 void EMSdevice::generate_values_web_customization(JsonArray output) {
     for (auto & dv : devicevalues_) {
         // also show commands and entities that have an empty fullname
