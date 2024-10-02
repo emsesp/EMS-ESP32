50b22dd265c2649cdfb63684adeca117acbde69a
proddy <paul@derbyshire.nl>
2024-10-02 13:55:34 +0200

    fix condition after adding a time

--- interface/src/app/main/SchedulerDialog.tsx
+++ interface/src/app/main/SchedulerDialog.tsx
@@ -316,7 +316,7 @@ const SchedulerDialog = ({
                   }
                   multiline
                   fullWidth
-                  value={editItem.time === '00:00'}
+                  value={editItem.time === '00:00' ? '' : editItem.time}
                   margin="normal"
                   onChange={updateFormValue}
                 />
