397f3f546ecd1aa26fd9f6725e23505b6ced1fda
proddy <paul@derbyshire.nl>
2024-12-31 12:15:59 +0100

    fix webUI tabs (underline)

--- interface/src/components/routing/index.ts
+++ interface/src/components/routing/index.ts
@@ -2,5 +2,3 @@ export { default as RouterTabs } from './RouterTabs';
 export { default as RequireAdmin } from './RequireAdmin';
 export { default as RequireAuthenticated } from './RequireAuthenticated';
 export { default as RequireUnauthenticated } from './RequireUnauthenticated';
-
-export * from './useRouterTab';
