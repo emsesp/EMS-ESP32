5482937332f8d75d1589a57edcb10bae98df8aad
proddy <paul@derbyshire.nl>
2025-01-16 21:55:51 +0100

    detect browser language if non set

--- interface/src/App.tsx
+++ interface/src/App.tsx
@@ -4,28 +4,46 @@ import { Slide, ToastContainer } from 'react-toastify';
 import AppRouting from 'AppRouting';
 import CustomTheme from 'CustomTheme';
 import TypesafeI18n from 'i18n/i18n-react';
-import { detectLocale } from 'i18n/i18n-util';
+import type { Locales } from 'i18n/i18n-types';
 import { loadLocaleAsync } from 'i18n/i18n-util.async';
-import { localStorageDetector } from 'typesafe-i18n/detectors';
+import { detectLocale, navigatorDetector } from 'typesafe-i18n/detectors';
 
-const detectedLocale = detectLocale(localStorageDetector);
+const availableLocales = [
+  'de',
+  'en',
+  'it',
+  'fr',
+  'nl',
+  'no',
+  'pl',
+  'sk',
+  'sv',
+  'tr',
+  'cz'
+];
 
 const App = () => {
   const [wasLoaded, setWasLoaded] = useState(false);
+  const [locale, setLocale] = useState<Locales>('en');
 
   useEffect(() => {
-    void loadLocaleAsync(detectedLocale).then(() => setWasLoaded(true));
+    // determine locale, take from session if set other default to browser language
+    const browserLocale = detectLocale('en', availableLocales, navigatorDetector);
+    const newLocale = (localStorage.getItem('lang') || browserLocale) as Locales;
+    localStorage.setItem('lang', newLocale);
+    setLocale(newLocale);
+    void loadLocaleAsync(newLocale).then(() => setWasLoaded(true));
   }, []);
 
   if (!wasLoaded) return null;
 
   return (
-    <TypesafeI18n locale={detectedLocale}>
+    <TypesafeI18n locale={locale}>
       <CustomTheme>
         <AppRouting />
         <ToastContainer
           position="bottom-left"
-          autoClose={3000}
+          autoClose={2000}
           hideProgressBar={false}
           newestOnTop={false}
           closeOnClick={true}
