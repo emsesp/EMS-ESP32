648675d002ceee06ec6a68a62f6276b513481bce
proddy <paul@derbyshire.nl>
2025-01-18 16:10:44 +0100

    auto-formatting

--- interface/src/app/status/HardwareStatus.tsx
+++ interface/src/app/status/HardwareStatus.tsx
@@ -100,7 +100,12 @@ const HardwareStatus = () => {
               data.cpu_freq_mhz +
               ' Mhz' +
               // bit of a hack : if the CPU temp is higher than 90 (=32 Fahrenheit if using Celsius), show F, otherwise C
-              (data.temperature ? ', T: ' + data.temperature + ' °' + (data.temperature > 90 ? 'F' : 'C') : '')
+              (data.temperature
+                ? ', T: ' +
+                  data.temperature +
+                  ' °' +
+                  (data.temperature > 90 ? 'F' : 'C')
+                : '')
             }
           />
         </ListItem>
