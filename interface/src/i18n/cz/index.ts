582fb3d72f3c8118f0de24eaf31bef64e9dc59e0
Marek Tyburec <marek.tyburec@gmail.com>
2025-02-22 08:19:16 +0100

    Update CZ

--- interface/src/i18n/cz/index.ts
+++ interface/src/i18n/cz/index.ts
@@ -351,7 +351,7 @@ const cz: Translation = {
   RELEASE_TYPE: 'Typ sestavení',
   REINSTALL: 'Přeinstalovat',
   INTERNET_CONNECTION_REQUIRED: 'Pro automatickou kontrolu a instalaci aktualizací je třeba internetové připojení',
-  DAYS_AGO: '{0} dn{{y|ní|ní|ní|ní|ní}} zpátky'
+  DAYS_AGO: '{0} dn{{y|í|í|í|í|í}} zpátky'
 };
 
 export default cz;
