2375633bca7cc62e0b76235a63bd39bbe50eb95c
proddy <paul@derbyshire.nl>
2025-11-24 22:35:07 +0100

    don't show confusing release type checkboxes

--- interface/src/app/status/Version.tsx
+++ interface/src/app/status/Version.tsx
@@ -511,48 +511,11 @@ const Version = () => {
             <Grid size={{ xs: 4, md: 2 }}>
               <Typography color="secondary">{LL.RELEASE_TYPE()}</Typography>
             </Grid>
-            <Grid size={{ xs: 8, md: 10 }}>
-              <FormControlLabel
-                disabled={!isDev}
-                control={
-                  <Checkbox
-                    sx={{
-                      '&.Mui-checked': {
-                        color: 'lightblue'
-                      }
-                    }}
-                  />
-                }
-                slotProps={{
-                  typography: {
-                    color: 'grey'
-                  }
-                }}
-                checked={!isDev}
-                label={LL.STABLE()}
-                sx={{ '& .MuiSvgIcon-root': { fontSize: 16 } }}
-              />
-              <FormControlLabel
-                disabled={isDev}
-                control={
-                  <Checkbox
-                    sx={{
-                      '&.Mui-checked': {
-                        color: 'lightblue'
-                      }
-                    }}
-                  />
-                }
-                slotProps={{
-                  typography: {
-                    color: 'grey'
-                  }
-                }}
-                checked={isDev}
-                label={LL.DEVELOPMENT()}
-                sx={{ '& .MuiSvgIcon-root': { fontSize: 16 } }}
-              />
-            </Grid>
+            {isDev ? (
+              <Typography>{LL.DEVELOPMENT()}</Typography>
+            ) : (
+              <Typography>{LL.STABLE()}</Typography>
+            )}
           </Grid>
 
           {internetLive ? (
